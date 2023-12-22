0775091dbbd6b6a82c660942fa18a97ab2623de2
Kamil Włodarczyk <kamil.limanowa@gmail.com>
2023-12-22 01:10:23 +0100

    feat(#10): pause menu and loading screen

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -11,19 +11,19 @@ namespace Cosmobot
 
         public TMP_Dropdown resolutionDropdown;
 
-        private Resolution[] resolutions;
+        private Resolution[] _resolutions;
         
         private void Start()
         {
-            resolutions = Screen.resolutions;
+            _resolutions = Screen.resolutions;
             resolutionDropdown.ClearOptions();
             List<string> options = new List<string>();
             int currentResolutionIndex = 0;
-            for (int i = 0; i < resolutions.Length; i++)
+            for (int i = 0; i < _resolutions.Length; i++)
             {
-                options.Add(resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRateRatio + "hz");
-                if (resolutions[i].width == Screen.currentResolution.width
-                    && resolutions[i].height == Screen.currentResolution.height)
+                options.Add(_resolutions[i].width + " x " + _resolutions[i].height + " @ " + _resolutions[i].refreshRateRatio + "hz");
+                if (_resolutions[i].width == Screen.currentResolution.width
+                    && _resolutions[i].height == Screen.currentResolution.height)
                 {
                     currentResolutionIndex = i;
                 }
@@ -35,7 +35,7 @@ namespace Cosmobot
 
         public void SetResolution(int resolutionIndex)
         {
-            Resolution resolution = resolutions[resolutionIndex];
+            Resolution resolution = _resolutions[resolutionIndex];
             Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
         }
 
