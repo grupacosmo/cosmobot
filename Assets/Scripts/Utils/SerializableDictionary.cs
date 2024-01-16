1cdbf4dad1c3e0c35037a1712635978dc914bf18
Patryk Paluch <patrykas1000@gmail.com>
2024-01-16 16:31:13 +0100

    feat(#17): add utils (#18)

---
+++ Assets/Scripts/Utils/SerializableDictionary.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Cosmobot
+{
+    [System.Serializable]
+    public class SerializableDictionary<TKey, TValue> :
+        IDictionary<TKey, TValue>,
+        IReadOnlyDictionary<TKey, TValue>
+    {
+        [SerializeField]
+        private List<TKey> keys = new();
+        [SerializeField]
+        private List<TValue> values = new();
+
+        public ICollection<TKey> Keys => keys.AsReadOnly();
+
+        public ICollection<TValue> Values => values.AsReadOnly();
+
+        public int Count => keys.Count;
+
+        public bool IsReadOnly => false;
+
+        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => keys.AsReadOnly();
+
+        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => values.AsReadOnly();
+
+        public SerializableDictionary() { }
+        public SerializableDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+            foreach (KeyValuePair<TKey, TValue> pair in dictionary)
+            {
+                Add(pair.Key, pair.Value);
+            }
+        }
+
+
+        public TValue GetValue(TKey key)
+        {
+            if (TryGetValue(key, out TValue value))
+            {
+                return value;
+            }
+            throw new KeyNotFoundException($"Key '{key}' not found in dictionary!");
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return values.Contains(value);
+        }
+
+        public TValue this[TKey key]
+        {
+            get => GetValue(key);
+            set => Add(key, value);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            int keyIndex = keys.IndexOf(key);
+            if (keyIndex != -1)
+            {
+                values[keyIndex] = value;
+            }
+            else
+            {
+                keys.Add(key);
+                values.Add(value);
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return keys.Contains(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            int keyIndex = keys.IndexOf(key);
+            if (keyIndex != -1)
+            {
+                keys.RemoveAt(keyIndex);
+                values.RemoveAt(keyIndex);
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int keyIndex = keys.IndexOf(key);
+            if (keyIndex != -1)
+            {
+                value = values[keyIndex];
+                return true;
+            }
+            value = default;
+            return false;
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return keys.Contains(item.Key) && values.Contains(item.Value);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            if (array is null) throw new System.ArgumentNullException(nameof(array));
+            if (arrayIndex < 0) throw new System.ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (array.Length - arrayIndex < Count) throw new System.ArgumentException("Array is too small!");
+
+            for (int i = 0; i < Count; i++)
+            {
+                array[arrayIndex + i] = new KeyValuePair<TKey, TValue>(keys[i], values[i]);
+            }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return ContainsValue(item.Value) && Remove(item.Key);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return keys.Zip(values, (key, value) => new KeyValuePair<TKey, TValue>(key, value)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
---
+++ Assets/Tests/SerializableDictionaryTest.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Cosmobot
+{
+    public class SerializableDictionaryTest
+    {
+        [Test]
+        public void AddPairsAndKeepKeyValueOrder()
+        {
+            SerializableDictionary<string, string> dict = new();
+            Assert.IsEmpty(dict.Keys);
+            Assert.IsEmpty(dict.Values);
+
+            dict.Add("key1", "value1");
+            dict.Add("key2", "value2");
+            dict["key3"] = "value3";
+
+            Assert.That(dict.Keys, Is.EquivalentTo(new[] { "key1", "key2", "key3" }));
+            Assert.That(dict.Values, Is.EquivalentTo(new[] { "value1", "value2", "value3" }));
+        }
+
+        [Test]
+        public void RemoveKey()
+        {
+            SerializableDictionary<string, string> dict = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" },
+                { "key3", "value3" }
+            };
+
+            dict.Remove("key2");
+
+            Assert.That(dict.Keys, Is.EquivalentTo(new[] { "key1", "key3" }));
+            Assert.That(dict.Values, Is.EquivalentTo(new[] { "value1", "value3" }));
+
+            Assert.False(dict.Remove("xxxx"));
+
+            Assert.True(dict.Remove("key1"));
+            Assert.True(dict.Remove("key3"));
+            Assert.False(dict.Remove("key3"));
+        }
+
+        [Test]
+        public void ReadValues()
+        {
+            SerializableDictionary<string, string> dict = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" },
+                { "key3", "value3" }
+            };
+
+            Assert.AreEqual(dict["key1"], "value1");
+            Assert.AreEqual(dict["key2"], "value2");
+            Assert.AreEqual(dict["key3"], "value3");
+
+            Assert.Throws<KeyNotFoundException>(() => dict.GetValue("xxxx"));
+
+            var enumerator = dict.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.AreEqual(enumerator.Current, new KeyValuePair<string, string>("key1", "value1"));
+            enumerator.MoveNext();
+            Assert.AreEqual(enumerator.Current, new KeyValuePair<string, string>("key2", "value2"));
+            enumerator.MoveNext();
+            Assert.AreEqual(enumerator.Current, new KeyValuePair<string, string>("key3", "value3"));
+            Assert.False(enumerator.MoveNext());
+        }
+
+    }
+}
