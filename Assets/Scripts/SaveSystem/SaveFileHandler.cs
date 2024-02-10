eb1a3f6b3ff6702f6849a66276d9050d19210eb3
Serferrr <98493305+Serferrr@users.noreply.github.com>
2024-02-10 14:09:29 +0100

    feat(#7): save system (#11)
    Co-authored-by: Jakub Ratajczak <113540258+Vie0n@users.noreply.github.com>
    Co-authored-by: Patryk Paluch <patrykas1000@gmail.com>
    Co-authored-by: Kamil Włodarczyk <kamil.limanowa@gmail.com>

---
+++ Assets/Scripts/SaveSystem/SaveFileHandler.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Cosmobot
+{
+    public class SaveFileHandler
+    {
+        private readonly string saveFilePath;
+
+        public SaveFileHandler(string save_file_name)
+        {
+            this.saveFilePath = Path.Combine(
+                Application.persistentDataPath,
+                save_file_name + ".json"
+            );
+            Directory.CreateDirectory(Application.persistentDataPath);
+        }
+
+        public void Save(GameData game_data)
+        {
+            // For now we'll just overwrite the file each time
+            try
+            {
+                File.WriteAllText(saveFilePath, JsonConvert.SerializeObject(game_data));
+                Debug.Log("Save file path: " + saveFilePath);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"Failed to create save file due to:  + {e.Message} {e.StackTrace}");
+            }
+        }
+
+        public GameData Load()
+        {
+            if (File.Exists(saveFilePath))
+            {
+                try
+                {
+                    string json = File.ReadAllText(saveFilePath);
+                    return JsonConvert.DeserializeObject<GameData>(json);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError(
+                        $"Failed to load save file due to:  + {e.Message} {e.StackTrace}"
+                    );
+                }
+            }
+            return null;
+        }
+    }
+}
