54ad4b91fbe7ed7ad8d7b6515dc435dbc24a1ac9
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2024-02-13 19:25:15 +0100

    switched to rigidbody from charactercontroller
    also added a few more things to movement test scene

--- Assets/Scripts/PlayerCharacter/PlayerController.cs
+++ Assets/Scripts/PlayerCharacter/PlayerController.cs
@@ -10,16 +10,21 @@ namespace Cosmobot
         public float acceleration;
         public float jumpForce;
         public float gravity;
+        public float maxFloorAngleDegrees;
 
-        private Vector3 input_move = Vector3.zero;
-        private bool input_jump = false;
-        private Vector3 velocity = Vector3.zero;
+        private Vector3 inputMove = Vector3.zero;
+        private bool inputJump = false;
 
-        private CharacterController cc;
+        private bool isGrounded = false;
+        private Vector3 groundNormal = Vector3.up;
+
+        private Rigidbody rb;
+        private Transform groundCheckOrigin;
 
         private void Start()
         {
-            cc = GetComponent<CharacterController>();
+            rb = GetComponent<Rigidbody>();
+            groundCheckOrigin = transform.Find("GroundCheckOrigin");
         }
 
         private void Update()
@@ -29,30 +34,46 @@ namespace Cosmobot
 
         private void FixedUpdate()
         {
+            GroundCheck();
             ProcessMovement();
         }
         
         private void HandleInput()
         {
-            input_move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-            input_jump = Input.GetKeyDown(KeyCode.Space) || input_jump;
+            inputMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+            inputJump = Input.GetKeyDown(KeyCode.Space) || inputJump;
         }
 
         private void ProcessMovement()
         {
-            velocity = cc.velocity;
+            Vector3 targetVelocity = inputMove * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+            Vector3 neededForce = Vector3.MoveTowards(rb.velocity, targetVelocity, 
+                acceleration * Time.fixedDeltaTime) - rb.velocity;
+
+            bool shouldJump = inputJump && isGrounded;
+            inputJump = false;
+            neededForce.y = shouldJump ? jumpForce : neededForce.y - gravity * Time.fixedDeltaTime;
             
-            Vector3 targetVelocity = input_move * moveSpeed + new Vector3(0, velocity.y, 0);
-            velocity = Vector3.MoveTowards(velocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+            rb.AddForce(neededForce, ForceMode.VelocityChange);
+        }
 
-            velocity.y -= gravity * Time.fixedDeltaTime;
-            if (input_jump && cc.isGrounded)
+        private void GroundCheck()
+        {
+            float radius = 0.45f;
+            Vector3 origin = groundCheckOrigin.position;
+            RaycastHit hitInfo;
+            Physics.SphereCast(origin, radius, Vector3.down, out hitInfo, 0.6f);
+            if (hitInfo.collider != null)
             {
-                velocity.y = jumpForce;
+                groundNormal = hitInfo.normal;
+                float floorAngleDegrees = Mathf.Acos(Vector3.Dot(Vector3.up, groundNormal)) * Mathf.Rad2Deg;
+                isGrounded = floorAngleDegrees <= maxFloorAngleDegrees;  
             }
-            input_jump = false;
-
-            cc.Move(velocity * Time.fixedDeltaTime);
+            else
+            {
+                isGrounded = false;
+            }
+            
         }
     }
 }
