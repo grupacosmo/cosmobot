cdd331caa8f01557b5aaa5ea58d76095f7b426aa
Kamil Włodarczyk <kamil.limanowa@gmail.com>
2024-02-15 15:25:37 +0100

    feat(#19): general code refactor

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 namespace Cosmobot
 {
-    public class Camera : MonoBehaviour
+    public class PlayerCamera : MonoBehaviour
     {
         public float sensX;
         public float sensY;
         public float cameraChangeY;
         public float cameraChangeZ;
         public KeyCode cameraChangeKey;
+        public Transform playerObject;
+        public Transform cameraHolder;
         private float _xRotation;
         private float _yRotation;
         private bool _isFirstPerson = true;
-        public Transform playerObject;
-        public Transform cameraHolder;
         
         private void Start()
         {
@@ -26,12 +26,23 @@ namespace Cosmobot
         {
             var mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
             var mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+            ChangeRotationValues(mouseX, mouseY);
+            RotateCamera();
+            if (Input.GetKeyDown(cameraChangeKey))
+            {
+                SwitchCameraPosition();
+            }
+        }
 
+        private void ChangeRotationValues(float mouseX, float mouseY)
+        {
             _yRotation += mouseX;
             _xRotation -= mouseY;
-
             _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
-
+        }
+        
+        private void RotateCamera()
+        {
             if (_isFirstPerson)
             {
                 transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
@@ -41,11 +52,6 @@ namespace Cosmobot
                 cameraHolder.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
             }
             playerObject.rotation = Quaternion.Euler(0, _yRotation, 0);
-
-            if (Input.GetKeyDown(cameraChangeKey))
-            {
-                SwitchCameraPosition();
-            }
         }
         
         private void SwitchCameraPosition()
