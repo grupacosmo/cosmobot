0a89fefecc6c4e247e617d63b15fc6fe96ae5c45
Kacper Lewandowski <kacperlewandowski123@wp.pl>
2024-04-07 20:14:04 +0200

    Battery charging

---
+++ Assets/Scripts/Battery/BatteryStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cosmobot
+{
+    public class BatteryStats : MonoBehaviour
+    {
+        public float maxCapacity = 20f;
+        public float currentCapacity = 0f;
+        public float transferRate = 1f;
+    }
+}
---
+++ Assets/Scripts/Battery/ReceiverStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cosmobot
+{
+    public class ReceiverStats : MonoBehaviour
+    {
+        public float maxCapacity = 100f;
+        public float currentCapacity = 0;
+    }
+}
