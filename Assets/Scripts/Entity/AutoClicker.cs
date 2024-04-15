850ff9abd03aa179abf64df8c0205317a94aff1e
Serfer <98493305+Serferrr@users.noreply.github.com>
2024-04-15 22:53:05 +0200

    Basic crafting recipies

---
+++ Assets/Scripts/Entity/AutoClicker.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cosmobot
+{
+    public class AutoClicker : MonoBehaviour
+    {
+        [SerializeField] private float clickInterval = 1f;
+        private IInteractable target;
+
+        private float currentClickInterval = 1f;
+
+        private void Awake()
+        {
+            target = GetComponent<IInteractable>();
+        }
+
+        void Update()
+        {
+            currentClickInterval -= Time.deltaTime;
+            if (currentClickInterval <= 0)
+            {
+                target.Use();
+                currentClickInterval = clickInterval;
+            }
+        }
+    }
+}
