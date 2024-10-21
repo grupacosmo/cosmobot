133014cdeb20656e8d96d4ec12b5b368a90453c8
Acu1000 <acu1000.developer@gmail.com>
2024-10-22 00:51:03 +0200

    added box-casted building placement scan

--- Assets/Scripts/Construction/PlayerConstructionHandler.cs
+++ Assets/Scripts/Construction/PlayerConstructionHandler.cs
@@ -10,6 +10,7 @@ namespace Cosmobot
         [SerializeField] Transform cameraTransform;
         [SerializeField] Transform playerTransform;
         [SerializeField] Transform buildPointIndicator;
+        [SerializeField] Transform buildPreview;
         [SerializeField] LayerMask buildTargetingCollisionMask;
         [SerializeField] float maxBuildDistance = 20.0f;
         [SerializeField] float maxTerrainHeight = 100.0f;
@@ -17,10 +18,11 @@ namespace Cosmobot
 
         void LateUpdate()
         {
-            Vector3 buildPoint = SnapToGrid(GetBuildPoint(), false, false);
+            /*Vector3 buildPoint = SnapToGrid(GetBuildPoint(), false, false);
             Ray skyRay = new Ray(buildPoint + Vector3.up * maxTerrainHeight, Vector3.down);
             bool skyRaySuccess = Physics.Raycast(skyRay, out RaycastHit skyRayHit, maxTerrainHeight*2, buildTargetingCollisionMask);
-            buildPointIndicator.position = skyRayHit.point;
+            buildPointIndicator.position = skyRayHit.point;*/
+            ScanBuildingPlacement(GetBuildPoint(), new Vector2Int(2,2));
         }
 
         private Vector3 GetBuildPoint() {
@@ -40,6 +42,23 @@ namespace Cosmobot
             newVec *= gridSize;
 
             return newVec + new Vector3(centerX ? gridSize/2.0f : 0, 0, centerZ ? gridSize/2.0f : 0);
+        }
+
+        private Vector3? ScanBuildingPlacement(Vector3 buildPoint, Vector2Int buildingGridSize) {
+            Vector3 boxOrigin = new Vector3(buildPoint.x, maxTerrainHeight+1, buildPoint.z);
+            Vector3 boxHalfExtents = new Vector3(buildingGridSize.x * gridSize * 0.5f, 1, buildingGridSize.y * gridSize * 0.5f);
+
+            bool success = Physics.BoxCast(boxOrigin, boxHalfExtents, Vector3.down, out RaycastHit result, Quaternion.identity, maxTerrainHeight*2, buildTargetingCollisionMask);
+            if (!success) {
+                buildPreview.position = new Vector3(0, 10000, 0);
+                return null;
+            }
+
+            buildPreview.localScale = new Vector3(buildingGridSize.x * gridSize, 1, buildingGridSize.y * gridSize);
+
+            buildPreview.position = new Vector3(buildPoint.x, result.point.y+0.5f, buildPoint.z);
+
+            return null;
         }
     }
 }
