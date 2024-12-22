88536be253c6eb9a2d7baaa86d9d544eb522500d
Miłosz <43696037+strunk23@users.noreply.github.com>
2024-12-22 22:49:10 +0100

    feat(#69): building construction (#78)
    Co-authored-by: Acu1000 <140196544+Acu1000@users.noreply.github.com>
    Co-authored-by: Acu1000 <acu1000.developer@gmail.com>

--- Assets/Scripts/Buildings/ConstructionSite/ConstructionSite.cs
+++ Assets/Scripts/Buildings/ConstructionSite/ConstructionSite.cs
@@ -1,11 +1,45 @@
 using UnityEngine;
 using Cosmobot.BuildingSystem;
+using Cosmobot.ItemSystem;
+using System.Collections.Generic;
 
 namespace Cosmobot
 {
     public class ConstructionSite : BaseBuilding
     {    
         [SerializeField] private Transform tempCube;
+        [SerializeField] private SerializableDictionary<ItemInfo, GameObject> constructionSiteResources = new();
+        [SerializeField] private BuildingInfo buildingInfo;
+        public Transform TempCube => tempCube;
+        public BuildingInfo BuildingInfo => buildingInfo;
+        public SerializableDictionary<ItemInfo, GameObject> ConstructionSiteResources => constructionSiteResources;
+
+        void Start() {
+            SetRequiredResources(buildingInfo.ResourceRequirements);
+        }
+
+        public void SetRequiredResources(IReadOnlyDictionary<ItemInfo, int> requiredResources) {
+            int previewCount = 0;
+            GameObject[] previewObjects = new GameObject[requiredResources.Count];
+            foreach (KeyValuePair<ItemInfo, int> pair in requiredResources) {
+                GameObject preview = GetComponent<ConstructionSiteResourcePreview>().CreateResourcePreview((SerializableDictionary<ItemInfo, int>)requiredResources, previewCount);
+                preview.transform.SetParent(transform);
+                preview.GetComponent<ResourcePreviewController>().SetRequirement(pair.Value);
+                previewObjects.SetValue(preview, previewCount);
+                constructionSiteResources[pair.Key] = preview;
+                previewCount++;
+            }
+            GetComponent<ConstructionSiteResourcePreview>().SetPreviewObjects(previewObjects);
+        }
+
+        public void DecreaseResourceRequirement(ItemInfo resourceToDecrease, int resourceAmount = 1) {
+            GameObject previewToDecrease = constructionSiteResources[resourceToDecrease];
+            ResourcePreviewController resourcePreviewController = previewToDecrease.GetComponent<ResourcePreviewController>();
+            if (resourcePreviewController.ResourceRequirement > 0) {
+                resourcePreviewController.DecreaseRequirement(resourceAmount);
+                IsReadyToBuild(buildingInfo.Prefab);
+            }
+        }
 
         public void Initialize(BuildingInfo newBuildingInfo) {
             buildingInfo = newBuildingInfo;
@@ -14,6 +48,18 @@ namespace Cosmobot
 
         public void FinishConstruction() {
             Instantiate(buildingInfo.Prefab, transform.position, transform.rotation); // TODO: set instantiation parent
+            Despawn();
+        }
+
+        public void IsReadyToBuild(GameObject targetBuilding) {
+            foreach (GameObject resource in GetComponent<ConstructionSiteResourcePreview>().PreviewObjects)
+                if (resource.GetComponent<ResourcePreviewController>().ResourceRequirement > 0) return;
+            
+            Build(targetBuilding);
+        }
+
+        public void Build(GameObject targetBuilding) {
+            Instantiate(targetBuilding, transform.position, transform.rotation);
             Despawn();
         }
     }
---
+++ Assets/Scripts/Buildings/ConstructionSite/ResourcePreviewController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Cosmobot
+{
+    public class ResourcePreviewController : MonoBehaviour
+    {
+        [SerializeField] private int resourceRequirement;
+
+        public int ResourceRequirement => resourceRequirement;
+
+        public void SetRequirement(int requirement) {
+            resourceRequirement = requirement;
+        }
+
+        public void DecreaseRequirement(int amount) {
+            resourceRequirement -= amount;
+            GetComponent<ResourceTextHandler>().UpdateText(resourceRequirement);
+        }
+    }
+}
