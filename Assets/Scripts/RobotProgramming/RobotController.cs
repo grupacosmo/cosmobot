27847a5c4c833125784dee998cd4b88e618d6a5b
Stanisław Leszczyński <staszkekun@gmail.com>
2025-03-21 16:32:49 +0100

    predefined wrappers for up to 6 params

--- Assets/Scripts/RobotProgramming/RobotController.cs
+++ Assets/Scripts/RobotProgramming/RobotController.cs
@@ -71,20 +71,20 @@ namespace Cosmobot
             //    token.ThrowIfCancellationRequested();
             //}));
 
-            jsEngine.SetValue("TurnLeft", (Action)(() =>
-            {
-                ExecuteOnMainThread(TurnLeft);
-                _taskCompletedEvent.WaitOne();
-                _taskCompletedEvent.Reset();
-                token.ThrowIfCancellationRequested();
-            }));
+            //jsEngine.SetValue("TurnLeft", (Action)(() =>
+            //{
+            //    ExecuteOnMainThread(TurnLeft);
+            //    _taskCompletedEvent.WaitOne();
+            //    _taskCompletedEvent.Reset();
+            //    token.ThrowIfCancellationRequested();
+            //}));
 
-            jsEngine.SetValue("TurnRight", (Action)(() => {
-                ExecuteOnMainThread(TurnRight);
-                _taskCompletedEvent.WaitOne();
-                _taskCompletedEvent.Reset();
-                token.ThrowIfCancellationRequested();
-            }));
+            //jsEngine.SetValue("TurnRight", (Action)(() => {
+            //    ExecuteOnMainThread(TurnRight);
+            //    _taskCompletedEvent.WaitOne();
+            //    _taskCompletedEvent.Reset();
+            //    token.ThrowIfCancellationRequested();
+            //}));
 
             //jsEngine.SetValue("GetRobotSpeed", (Func<float>)(() => {
             //    float returnValue = 0;
@@ -95,16 +95,20 @@ namespace Cosmobot
             //    return returnValue;
             //}));
 
-            jsEngine.SetValue("MoveForward", (Action)(() => { Wrap<object>((Action)(() => StartCoroutine(MoveToPoint(transform.position + transform.forward)))); }));
+            jsEngine.SetValue("TurnLeft", Wrap(TurnLeft));
+            jsEngine.SetValue("TurnRight", Wrap(TurnRight));
+            jsEngine.SetValue("MoveForward", Wrap(MoveForward));
+            jsEngine.SetValue("Seek", Wrap(Seek));
+            jsEngine.SetValue("MoveToPoint", Wrap<float,float,float>((x,y,z) => MoveToPoint(x,y,z)));
+            jsEngine.SetValue("GetRobotSpeed", Wrap<float>(GetRobotSpeed));
+            jsEngine.SetValue("D5", Wrap<float, float>((x) => d5(x)));
 
-            jsEngine.SetValue("Seek", (Action)(() => { Wrap<object>((Action)(() => StartCoroutine(Seek()))); }));
-
-            jsEngine.SetValue("MoveToPoint", (Action<float, float, float>)((x,y,z) => { Wrap<object>((Action<object[]>)((p) => StartCoroutine(MoveToPoint((Vector3)p[0]))), new Vector3(x,y,z)); }));
+            //jsEngine.SetValue("MoveForward", (Action)(() => { Wrap<object>((Action)(() => StartCoroutine(MoveToPoint(transform.position + transform.forward)))); }));
+            //jsEngine.SetValue("Seek", (Action)(() => { Wrap<object>((Action)(() => StartCoroutine(Seek()))); }));
+            //jsEngine.SetValue("GetRobotSpeed", (Func<float>)(() => { return Wrap<float>((Func<float>)(GetRobotSpeed)); }));
+            //jsEngine.SetValue("D5", (Func<float, float>)((x) => { return Wrap<float>((Func<object[], float>)((p) => d5((float)p[0])), x); }));
+            //jsEngine.SetValue("MoveToPoint", (Action<float, float, float>)((x,y,z) => { Wrap<object>((Action<object[]>)((p) => StartCoroutine(MoveToPoint((Vector3)p[0]))), new Vector3(x,y,z)); }));
                                                 //  ^what js Function takes                ^return void         ^list of parameters                       ^where argument fit    ^list of arguments begin
-
-            jsEngine.SetValue("GetRobotSpeed", (Func<float>)(() => { return Wrap<float>((Func<float>)(GetRobotSpeed)); }));
-
-            jsEngine.SetValue("D5", (Func<float, float>)((x) => { return Wrap<float>((Func<object[], float>)((p) => d5((float)p[0])), x); }));
 
             try
             {
@@ -137,7 +141,7 @@ namespace Cosmobot
                 _mainThreadContext.Post(_ => action(), null);
             }
         }
-
+        /*
         private T Wrap<T>(Delegate function, params object[] parameters)
         {
             T returnValue = default(T);
@@ -152,7 +156,7 @@ namespace Cosmobot
             _cancellationTokenSource.Token.ThrowIfCancellationRequested();
 
             return returnValue;
-        }
+        }*/
 
         void Update()
         {
@@ -181,7 +185,17 @@ namespace Cosmobot
             return speed; //<-- pretend this is crazy unity calculation
         }
 
-        public IEnumerator MoveToPoint(Vector3 to)
+        public void MoveForward()
+        {
+            StartCoroutine(MoveToPointCoroutine(transform.position + transform.forward));
+        }
+
+        public void MoveToPoint(float x, float y, float z)
+        {
+            StartCoroutine(MoveToPointCoroutine(new Vector3(x,y,z)));
+        }
+
+        public IEnumerator MoveToPointCoroutine(Vector3 to)
         {
             while (transform.position != to)
             {
@@ -193,7 +207,12 @@ namespace Cosmobot
             _taskCompletedEvent.Set();
         }
 
-        public IEnumerator Seek()
+        public void Seek()
+        {
+            StartCoroutine(SeekCoroutine());
+        }
+
+        public IEnumerator SeekCoroutine()
         {
             while (transform.position != target.position)
             {
@@ -203,6 +222,161 @@ namespace Cosmobot
                 yield return null;
             }
             _taskCompletedEvent.Set();
+        }
+
+        //WRAPPERS
+        Action Wrap(Action action)
+        {
+            return () => {
+                ExecuteOnMainThread(action);
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T> Wrap<T>(Action<T> action)
+        {
+            return (t) => {
+                ExecuteOnMainThread(() => action(t));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T1, T2> Wrap<T1, T2>(Action<T1, T2> action)
+        {
+            return (t1, t2) => {
+                ExecuteOnMainThread(() => action(t1, t2));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T1, T2, T3> Wrap<T1, T2, T3>(Action<T1, T2, T3> action)
+        {
+            return (t1, t2, t3) => {
+                ExecuteOnMainThread(() => action(t1, t2, t3));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T1, T2, T3, T4> Wrap<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action)
+        {
+            return (t1, t2, t3, t4) => {
+                ExecuteOnMainThread(() => action(t1, t2, t3, t4));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T1, T2, T3, T4, T5> Wrap<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action)
+        {
+            return (t1, t2, t3, t4, t5) => {
+                ExecuteOnMainThread(() => action(t1, t2, t3, t4, t5));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Action<T1, T2, T3, T4, T5, T6> Wrap<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> action)
+        {
+            return (t1, t2, t3, t4, t5, t6) => {
+                ExecuteOnMainThread(() => action(t1, t2, t3, t4, t5, t6));
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+            };
+        }
+
+        Func<T> Wrap<T>(Func<T> action)
+        {
+            return () => {
+                T tr = default(T);
+                ExecuteOnMainThread(() => { tr = action(); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, TR> Wrap<T1, TR>(Func<T1, TR> action)
+        {
+            return (t1) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, T2, TR> Wrap<T1, T2, TR>(Func<T1, T2, TR> action)
+        {
+            return (t1, t2) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1, t2); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, T2, T3, TR> Wrap<T1, T2, T3, TR>(Func<T1, T2, T3, TR> action)
+        {
+            return (t1, t2, t3) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1, t2, t3); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, T2, T3, T4, TR> Wrap<T1, T2, T3, T4, TR>(Func<T1, T2, T3, T4, TR> action)
+        {
+            return (t1, t2, t3, t4) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1, t2, t3, t4); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, T2, T3, T4, T5, TR> Wrap<T1, T2, T3, T4, T5, TR>(Func<T1, T2, T3, T4, T5, TR> action)
+        {
+            return (t1, t2, t3, t4, t5) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1, t2, t3, t4, t5); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
+        }
+
+        Func<T1, T2, T3, T4, T5, T6, TR> Wrap<T1, T2, T3, T4, T5, T6, TR>(Func<T1, T2, T3, T4, T5, T6, TR> action)
+        {
+            return (t1, t2, t3, t4, t5, t6) => {
+                TR tr = default(TR);
+                ExecuteOnMainThread(() => { tr = action(t1, t2, t3, t4, t5, t6); });
+                _taskCompletedEvent.WaitOne();
+                _taskCompletedEvent.Reset();
+                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                return tr;
+            };
         }
     }
 }
